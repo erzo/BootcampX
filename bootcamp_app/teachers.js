7955b31b226cf193345a10b99a514f74b11737e4
Felipe Gonzalez <felipefagnilli@gmail.com>
2020-07-23 18:59:23 +0000

    parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,23 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT(teachers.name) AS teacher, cohorts.name AS cohort
-FROM teachers
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teachers.name;
-`)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM teachers
+  JOIN assistance_requests ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  ORDER BY teacher
+  LIMIT $2;
+  `;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
